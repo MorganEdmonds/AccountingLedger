c30bd4fb27f82351fcbf93cfb8be6dab830f3cd7
MorganEdmonds <morganedmonds@gmail.com>
2025-10-11 21:47:40 -0400

    Adding menus

---
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class HomeScreen {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String HomeMenu = """
+                ----HOME.MENU-----
+                D- Add Deposit
+                P- Make payment
+                L- Ledger Screen
+                X- Exit              
+
+
+
+    }
+}
---
+++ src/main/java/com/pluralsight/Ledger.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Ledger {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String Ledger = """
+            
+            ------Ledger.App------
+            
+            A- All
+            D- Deposit
+            P- Payments
+            R- reports
+            
+           
+         
+  
+              
+    }
+}
